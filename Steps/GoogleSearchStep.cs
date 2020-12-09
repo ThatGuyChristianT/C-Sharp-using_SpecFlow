622c43e3a96b4f2e382ab0f3ccc6b250fb459e31
Christian <ctabarangao@qaconsultants.com>
2020-12-09 08:12:43 -0500

    [*] Added ChromeDriver NPM [*] Added Some step definition to check if it's being linked properly

---
+++ Steps/GoogleSearchStep.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace GoogleTest.Steps
+{
+    [Binding]
+    class GoogleSearchStep
+    {
+        [Given(@"I navigate to website '(.*)'")]
+        public void NavigateBrowser(string browser)
+        {
+
+        }
+
+        [Then(@"I see Google page is loaded")]
+        public Boolean IsGooglePageLoaded()
+        {
+            return false;
+        }
+        //Given I navigate to website "https://www.google.com"
+        //Then I see Google page is loaded
+        //   When I enter "" on searchbox
+
+        //   And click on Search
+    }
+}
