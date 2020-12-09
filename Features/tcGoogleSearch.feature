e31955c374fcadbbd485d5db73e9c4f3c7391351
Christian <ctabarangao@qaconsultants.com>
2020-12-08 23:27:57 -0500

    Add project files.

---
+++ Features/tcGoogleSearch.feature
@@ -0,0 +1,10 @@
+﻿Feature: tcGoogleSearch
+		Simple google search
+
+@Browser:Chrome
+Scenario: Add two numbers
+	Given I navigate to website "https://www.google.com"
+	Then I see Google page is loaded
+	When I enter "" on searchbox
+	And click on Search
+	#Add step here 
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GoogleTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GoogleTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("96097ed1-e63f-4dfe-8b4a-546015de85ee")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
